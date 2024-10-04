4d260ad5aacf78770e4d361c6b0da1e7f47fd5c9
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-10-05 01:40:19 +0700

    JWT авторизация

--- kittens/views.py
+++ kittens/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.permissions import IsAuthenticated
 from .models import *
 from .serializers import *
 
