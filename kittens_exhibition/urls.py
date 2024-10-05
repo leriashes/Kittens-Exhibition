aad8221d78bcaea1fb8f72909ef204bc103ff40b
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-10-05 17:16:04 +0700

    Документация Swagger

--- kittens_exhibition/urls.py
+++ kittens_exhibition/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Kittens API",
+        default_version='v1',
+        description="Документация API для выставки котят",
+        contact=openapi.Contact(email="VNS2002@mail.ru")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('kittens.urls'))
+    path('api/', include('kittens.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
